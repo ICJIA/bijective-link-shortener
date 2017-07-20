8e83b3ad0571013697b594d7321eb0ad1c179a2a
cschweda <cschweda@gmail.com>
2017-07-20 07:39:04 -0500

    Remove old comments

--- test/index.js
+++ test/index.js
@@ -1,16 +1,6 @@
 'use strict'
 
 var shorten = require('../src/index.js')
-// module.myvar = 'Hello world';
-// module.test('test input');
-
-// var x = module.encode(4522323123123)
-// console.log(x)
-
-// console.log('Default alphabet: ', shorten.getAlphabet())
-// shorten.setAlphabet('0123456789abcdefghijkl')
-// console.log('Updated alphabet: ', shorten.getAlphabet())
-// console.log('Encoded: ', shorten.encode(123456778))
 
 var seed = 39234398498
 var base = shorten.getBase()
