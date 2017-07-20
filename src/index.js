bc413313f08d10abf89b8f6fbdc8d836ce3c6c07
cschweda <cschweda@gmail.com>
2017-07-20 09:01:22 -0500

    Add randomize alphabet method

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@
 
 module.exports = {
   alphabet: '123456789abcdefghijkmnopqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ',
+
   setAlphabet: function (str) {
     this.alphabet = str
   },
@@ -11,28 +12,46 @@ module.exports = {
   getBase: function () {
     return this.alphabet.length
   },
+  isInteger: function (num) {
+    return Number.isInteger(num)
+  },
+  isPositive: function (num) {
+    return (num > 0)
+  },
+  randomizeAlphabet: function () {
+    var shuffled = this.alphabet.split('').sort(function () { return 0.5 - Math.random() }).join('')
+    this.alphabet = shuffled
+    return shuffled
+  },
   encode: function (num) {
     // String length = 58 (no zero, no uppercase 'O', no 'I', no 'l')
-    var alphabet = this.alphabet
-    // console.log(alphabet.length)
-    var base = alphabet.length
+    var _alphabet = this.alphabet
+    if (!this.isInteger(num)) {
+      throw new Error('The seed number must be an integer > 0')
+    }
+
+    if (!this.isPositive(num)) {
+      throw new Error('The seed number must be an integer > 0')
+    }
+
+    var _base = _alphabet.length
     var encoded = ''
     while (num) {
-      var remainder = num % base
-      num = Math.floor(num / base)
-      encoded = alphabet[remainder].toString() + encoded
+      var remainder = num % _base
+      num = Math.floor(num / _base)
+      encoded = _alphabet[remainder].toString() + encoded
     }
     return encoded
   },
   decode: function (str) {
-    var decoded = 0
-    var base = this.alphabet.length
+    var _decoded = 0
+    var _base = this.alphabet.length
     while (str) {
-      var index = this.alphabet.indexOf(str[0])
-      var power = str.length - 1
-      decoded += index * (Math.pow(base, power))
+      var _index = this.alphabet.indexOf(str[0])
+      var _power = str.length - 1
+      _decoded += _index * (Math.pow(_base, _power))
       str = str.substring(1)
     }
-    return decoded
+    return _decoded
   }
 }
